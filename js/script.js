1610677948a9a1f2e1da0c79833198445c6e696a
Ulisses Ferreira <ulissesvf@gmail.com>
2018-04-23 12:14:05 +0100

    chore: organize files correctly

--- js/script.js
+++ js/script.js
@@ -15,3 +15,17 @@ function openCity(evt, tabName) {
 
 
 document.getElementById("defaultOpen").click();
+
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    m = checkTime(m);
+    document.getElementById('clock').innerHTML =
+    h + ":" + m;
+    var t = setTimeout(startTime, 500);
+}
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+}
